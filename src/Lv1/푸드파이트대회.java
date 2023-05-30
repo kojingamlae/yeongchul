74217d8da1038300b3d47008c79490d98af7b9ce
Yunyeong-Ko <koyunyeong0.6@gmail.com>
2023-05-30 19:58:33 +0900

    ADD :: [Lv.1] 푸드 파이트 대회

--- src/Lv1/푸드파이트대회.java
+++ src/Lv1/푸드파이트대회.java
@@ -3,8 +3,10 @@ package Lv1;
 public class 푸드파이트대회 {
     public String solution(int[] food) {
         String answer = "";
-        for (int i=1; i<food.length; i++) {
-            for (int j=0; j<food[i]/2; j++) {
+        for (int i=1; i<food.length; i++)
+        {
+            for (int j=0; j<food[i]/2; j++)
+            {
                 answer+= Integer.toString(i);
             }
         }

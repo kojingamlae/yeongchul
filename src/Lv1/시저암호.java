445a1c64bcd124f8cdf61536df0027cddc4ec81e
Yunyeong-Ko <koyunyeong0.6@gmail.com>
2023-06-15 06:01:31 +0900

    ADD :: [Lv.1] 시저 암호

--- src/Lv1/시저암호.java
+++ src/Lv1/시저암호.java
@@ -5,9 +5,9 @@ public class 시저암호 {
         String answer = "";
         for (int i=0;i<s.length();i++) {
             char ch = s.charAt(i);
-            if (ch<='Z') answer+=(char)('A'+(26+ch-'A'+n)%26);
-            else if (ch<='z') answer+=(char)('a'+(26+ch-'a'+n)%26);
-            else answer+=" ";
+            if (ch==' ') answer+=" ";
+            else if (ch<='Z'&&ch>='A') answer+=(char)('A'+(26+ch-'A'+n)%26);
+            else if (ch<='z'&&ch>='a') answer+=(char)('a'+(26+ch-'a'+n)%26);
         }
         return answer;
     }
